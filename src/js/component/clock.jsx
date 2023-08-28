1ed9abb08815f9e0e9561a4b2b2df8e52414bf16
Afonso Bernardes <84087794+AfonsoBernardes@users.noreply.github.com>
2023-08-28 19:20:44 +0000

    Added styling and clock item

--- src/js/component/clock.jsx
+++ src/js/component/clock.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Display from "./display";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const Clock = () => {
@@ -11,8 +14,11 @@ const Clock = () => {
 
     let timeArray = ("0".repeat(6 - time.toString().length) + time.toString()).split("");
 
-	return (
-        <div className="d-flex justify-content-center bg-dark justify-content-between p-5">
+	return (     
+        <div className="d-flex justify-content-center border rounded bg-dark justify-content-between p-5">
+            <div className="border rounded bg-white p-5">
+                <FontAwesomeIcon icon={faClock} size="6x" />
+            </div>  
             {timeArray.map((unit) => {
                 return (
                     <Display text={unit} />
--- src/js/component/display.jsx
+++ src/js/component/display.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { time } from "./clock.jsx";
 
 
 const Display = ({text}) => {
     
 	return (
-        <div className="border text-white">
-             <h1 className="fw-bolder fs-1">{text}</h1>
+        <div className="border rounded bg-white p-5">
+            <h1 className="display-1 fw-bolder">{text}</h1>
         </div>
     );
 };
